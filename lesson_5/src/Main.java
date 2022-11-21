db26c7a8055a15a98638093d9513d65bad1843c1
igorwarszawski3 <igorwarszawski3@gmail.com>
2022-11-21 17:38:10 +0100

    nicer main

--- lesson_5/src/Main.java
+++ lesson_5/src/Main.java
@@ -3,9 +3,15 @@ public class Main {
 
         Euro konto = new Euro(0);
         konto = konto.addedCurrency(1, "JPY");
-        System.out.println(konto.balance());
-        konto = konto.addedCurrency(55, "CHF");
-        System.out.println(konto.balance());
+        System.out.println(konto.balance() + konto.symbol());
+
+        konto = konto.addedCurrency(50, "USD");
+        System.out.println(konto.balance() + konto.symbol());
+
+        konto = konto.subtractedCurrency(50, "USD");
+        System.out.println(konto.balance() + konto.symbol());
+
+
 
 
     }

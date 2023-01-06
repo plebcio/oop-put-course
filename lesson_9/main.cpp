aa65c166e965895e0dc3372b68e099e2c292f427
igorwarszawski3 <igorwarszawski3@gmail.com>
2023-01-06 21:39:53 +0100

    better c++

--- lesson_9/main.cpp
+++ lesson_9/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <assert.h>
 #include "Game.h"
+#include <memory>
 
 int main(void)
 {
-    Game *game;
-    game = new FakeFootballGame();
+    auto game = std::unique_ptr<Game>( new FakeFootballGame );
 
     assert(game->result() == "1:0");
     std::cout << "OK" << std::endl;
-
     
     assert(game->result() == "2:0");
     std::cout << "OK" << std::endl;
